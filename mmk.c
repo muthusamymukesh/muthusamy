7657e2b9bc41d3388e0147f62efbd5a708efd839
muthusamymukesh <35652643+muthusamymukesh@users.noreply.github.com>
2018-01-21 15:08:40 +0530

    Create mmk.c

---
+++ mmk.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("enter the value a");
+scanf("%d",&a);
+if(a<=1)
+{
+printf("positive");
+}
+else(a=0)
+{
+printf("zero");
+}
+else
+{
+printf("negative");
+}
+getch();
+}
