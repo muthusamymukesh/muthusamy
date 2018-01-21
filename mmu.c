04184986b31c5426be6dc0a0711eb4dd30e4f609
muthusamymukesh <35652643+muthusamymukesh@users.noreply.github.com>
2018-01-21 16:07:41 +0530

    Update mmu.c

--- mmu.c
+++ mmu.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 void main()
 {
-int a[6]=[a,e,i,o,u],b;
+int a[6],b;
+a[1]='a',a[2]='e',a[3]='i',a[4]='o',a[5]='u';
 printf("enter the value b");
-scanf("%d",&b);
+scanf("%c",&b);
 if(a==b)
 {
 printf("vowel");
