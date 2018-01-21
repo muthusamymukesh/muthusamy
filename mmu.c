012525dfe3eeec30ef6e242e4da968b2ae0252a4
muthusamymukesh <35652643+muthusamymukesh@users.noreply.github.com>
2018-01-21 16:59:53 +0530

    Update mmu.c

--- mmu.c
+++ mmu.c
@@ -2,7 +2,7 @@
 void main()
 {
 char a[24],b[6];
-a[24]=='a',a[24]=='b',a[24]=='c',a[24]=='o',a[24]=='u';
+a[24]=='a',a[24]=='e',a[24]=='i',a[24]=='o',a[24]=='u';
 printf("enter the value b");
 scanf("%c",&b);
 if(a[24]==b[6])
