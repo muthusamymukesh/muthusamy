c5162ab1cb13d6cbd4ec2538b3fe445ab81f9483
muthusamymukesh <35652643+muthusamymukesh@users.noreply.github.com>
2018-01-21 15:30:17 +0530

    Update mmk.c

--- mmk.c
+++ mmk.c
@@ -1,16 +1,14 @@
 #include<stdio.h>
-#include<conio.h>
 void main()
 {
 int a;
-clrscr();
 printf("enter the value a");
 scanf("%d",&a);
 if(a<=1)
 {
 printf("positive");
 }
-else(a=0)
+elseif(a=0)
 {
 printf("zero");
 }
@@ -18,5 +16,4 @@ else
 {
 printf("negative");
 }
-getch();
 }
