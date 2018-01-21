6a85aeeb9d992aa9af26ca771cb5d297172155fb
muthusamymukesh <35652643+muthusamymukesh@users.noreply.github.com>
2018-01-21 15:50:32 +0530

    Update and rename mmk.c to mmu.c

--- mmu.c
+++ mmu.c
@@ -4,17 +4,12 @@ void main()
 int a;
 printf("enter the value a");
 scanf("%d",&a);
-if(a<0)
+if(a%2==0)
 {
-printf("positive");
-}
-else
-if(a=0)
-{
-printf("zero");
+printf("even");
 }
 else
 {
-printf("negative");
+printf("odd");
 }
 }
